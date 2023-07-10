79055dbd3991e8353af31a5d5cece77dc6f5e68d
Jeremiah H <37886897+britishninja47@users.noreply.github.com>
2023-07-10 18:24:50 -0400

    Add files via upload
    New files including player concept and canvas page for game.

---
+++ index.js
@@ -0,0 +1,65 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+
+  draw(){
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2,
+      false)
+      c.fillStyle = this.color
+      c.fill()
+
+  }
+
+}
+
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+
+  draw(){
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2,
+      false)
+      c.fillStyle = this.color
+      c.fill()
+
+  }
+
+}
+
+//Character Placement and detail//
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const player = new Player(x, y, 30, 'red')
+player.draw()
+
+console.log(player)
+
+addEventListener('click', (event) => {
+  const projectile = new Projectile(
+    event.clientX,
+    event.clientY,
+    5,
+    'blue',
+    null
+  )
+
+  projectile.draw()
+})
